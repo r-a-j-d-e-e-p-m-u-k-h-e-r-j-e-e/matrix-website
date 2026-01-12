3d93da81f618c56a97a93eef9e65aa748b02e264
Rajdeep Mukherjee <rajdeepm@umich.edu>
2026-01-11 20:35:03 -0500

    Configure custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/matrix-website/" : "/",
+export default defineConfig(() => ({
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
